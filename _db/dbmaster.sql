dd00e0aa8a1eea3bdc05f7acb0a355e62584d0bd
wildanbelfiore <mikewildan@outlook.com>
2022-09-13 19:37:17 +0700

    renamed:    _db/laravel_8_multiauth.sql -> _db/dbmaster.sql

--- _db/dbmaster.sql
+++ _db/dbmaster.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 12, 2021 at 01:15 PM
--- Server version: 10.4.8-MariaDB
--- PHP Version: 7.3.10
+-- Generation Time: Sep 13, 2022 at 09:29 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `laravel_8_multiauth`
+-- Database: `dbmaster`
 --
 
 -- --------------------------------------------------------
@@ -96,8 +95,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `role`, `favoriteColor`, `picture`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(2, 'Paul Julius Oleac', 'admin@gmail.com', 1, 'Red', 'UIMG_20210502608ea5b926248.jpg', NULL, '$2y$10$2Lp1RkhXSmcRkhpydFg2D.OcWNJ3S/uHFxzYhv1D7rTvgSbA/rmmi', NULL, '2021-04-12 12:52:31', '2021-05-02 12:00:14'),
-(15, 'User', 'user@gmail.com', 2, 'blue', '293101621939201_avatar.png', NULL, '$2y$10$cM903OrlVnXu.W5NDz7QQeQ.H.L1dDlAnOE1Ky0KdSU5y7r43OkcS', NULL, '2021-05-25 08:40:01', '2021-05-25 08:40:01');
+(16, 'Test1', 'test1@sipadv.com', 1, 'green', '214201662362515_avatar.png', NULL, '$2y$10$5TVdV2ZsACRBfb.EgbtGEuN9ssK66WDF3EnSfRVovt7qxBHemPHYO', NULL, '2022-09-05 00:21:55', '2022-09-05 00:21:55');
 
 --
 -- Indexes for dumped tables
@@ -149,7 +147,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
