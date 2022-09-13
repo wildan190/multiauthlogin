801580cd50d3b9d620079649fb8aa5b1971f1ed8
wildanbelfiore <mikewildan@outlook.com>
2022-09-13 19:39:40 +0700

    modified:   resources/views/posts/create.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,7 +4,7 @@
 <div class="row mt-5 mb-5">
     <div class="col-lg-12 margin-tb">
         <div class="float-left">
-            <h2>Create New Post</h2>
+            <h2>New Client</h2>
         </div>
         <div class="float-right">
             <a class="btn btn-secondary" href="{{ route('posts.index') }}"> Back</a>
