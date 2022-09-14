6028925d614898cc2067e63cbbc0e4b7bfe6d5bd
wildanbelfiore <mikewildan@outlook.com>
2022-09-14 13:06:36 +0700

    modified:   resources/views/dashboards/admins/layouts/admin-dash-layout.blade.php

--- resources/views/dashboards/admins/layouts/admin-dash-layout.blade.php
+++ resources/views/dashboards/admins/layouts/admin-dash-layout.blade.php
@@ -51,7 +51,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Brand Logo -->
     <a href="{{ \URL::to('/')}}" class="brand-link">
       <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Your Site</span>
+      <span class="brand-text font-weight-light">Sinar Indah Padma</span>
     </a>
 
     <!-- Sidebar -->
