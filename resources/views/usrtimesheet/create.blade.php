99765096c5ca2da89d91d67003f275178447d767
wildanbelfiore <mikewildan@outlook.com>
2022-10-19 21:50:06 +0700

    modified:   .vscode/settings.json 	modified:   app/Http/Controllers/UserController.php 	new file:   resources/views/usrtimesheet/create.blade.php 	new file:   resources/views/usrtimesheet/edit.blade.php 	modified:   resources/views/usrtimesheet/index.blade.php

---
+++ resources/views/usrtimesheet/create.blade.php
@@ -0,0 +1,72 @@
+@extends('dashboards.users.layouts.user-dash-layout')
+@section('title','Add Record')
+@section('content')
+
+<form action="{{ route('usrtimesheet.store') }}" method="POST">
+    @csrf
+
+<div class="container">
+<div class="row md-5 mb-5">
+    <div class="col-lg-5 margin-top 0">
+        <div class="float-left">
+            <h1>Timesheet</h1>
+        </div>
+    </div>
+</div>
+    <div class="row">
+    <div class="col-sm">
+            <div class="form-group">
+                <strong>Name</strong>
+                <input type="text" name="nama" class="form-control" placeholder="Name">
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="form-group">
+                <strong>Tanggal</strong>
+                <input type="date" name="tanggal" class="form-control" placeholder="dd/mm/yy">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Proyek</strong>
+                <select type="text" class="form-control" name="proyek" placeholder="">
+                    <option style="background-color: #808080; color: white;">Select</option>
+                    <option>Sinarmas - Videotron</option>
+                    <option>Lippo Mal Karawaci - Billboard</option>
+                    <option>Ciputra - Billboard</option>
+                </select>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Place</strong>
+                <select type="text" class="form-control" name="tempat_kerja" placeholder="Work Place">
+                    <option style="background-color: #808080; color: white;">Select</option>
+                    <option>Office - Lengkong Karya</option>
+                    <option>Outside</option>
+                    <option>Work From Home</option>
+                </select>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Waktu</strong>
+                <input type="time" class="form-control" name="waktu" placeholder="" />
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="form-group">
+                <strong>Aktivitas</strong>
+                <input type="text" name="aktivitas" class="form-control" placeholder="Activity">
+            </div>
+        </div>
+       
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-success">Submit</button>
+        </div>
+    </div>
+</div>
+ 
+</form>
+
+@endsection
--- resources/views/usrtimesheet/index.blade.php
+++ resources/views/usrtimesheet/index.blade.php
@@ -1,5 +1,73 @@
 @extends('dashboards.users.layouts.user-dash-layout')
 @section('title','Timesheet')
 @section('content')
-<h1>Sedang dalam pengembangan</h1>
+<div class="container">
+<div class="row mt-0 mb-5">
+        <div class="col-lg-12 margin-tb">
+            <div class="float-left">
+                <h2>Timesheet</h2>
+            </div>
+            <div class="float-right">
+                <a class="btn btn-success" href="{{ route('usrtimesheet.create') }}"> Add New</a>
+            </div>
+        </div>
+</div>
+ 
+    @if ($message = Session::get('success'))
+    <div class="alert alert-success">
+        <p>{{ $message }}</p>
+    </div>
+    @endif
+
+    <form class="form" method="get" action="{{ route('search') }}">
+        <div class="form-group w-100 mb-3">
+            <label for="search" class="d-block mr-2">Pencarian</label>
+                <select type="text" name="search" class="form-control w-75 d-inline" id="search">
+                    <option>Muhamad Asep Wildan Muholadun</option>
+                    <option>User 1</option>
+                    <option>Admin 1</option>
+                </select>
+            <button type="submit" class="btn btn-primary mb-1">Cari</button>
+        </div>
+    </form>
+
+    <table class="table table-striped">
+    <thead class="thead-dark">
+        <tr>
+            <th width="20px" class="text-center">No</th>
+            <th>Name</th>
+            <th>Tanggal</th>
+            <th>Proyek</th>
+            <th>Tempat Kerja</th>
+            <th>Waktu</th>
+            <th>Aktivitas</th>
+            <th width="280px"class="text-center">Action</th>
+        </tr>
+    </thead>
+        @foreach ($usrtimesheet as $timesheet)
+        <tr>
+            <td class="text-center">{{ ++$i }}</td>
+            <td>{{ $timesheet->nama }}</td>
+            <td>{{ $timesheet->tanggal }}</td>
+            <td>{{ $timesheet->proyek }}</td>
+            <td>{{ $timesheet->tempat_kerja }}</td>
+            <td>{{ $timesheet->waktu }}</td>
+            <td>{{ $timesheet->aktivitas }}</td>
+            <td class="text-center">
+                <form action="{{ route('timesheet.destroy',$timesheet->id) }}" method="POST">
+ 
+                    <a class="btn btn-info btn-sm" href="{{ route('timesheet.show',$timesheet->id) }}">Show</a>
+ 
+                    <a class="btn btn-primary btn-sm" href="{{ route('timesheet.edit',$timesheet->id) }}">Edit</a>
+ 
+                    @csrf
+                    @method('DELETE')
+ 
+                    <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure to delete this data ?')">Delete</button>
+                </form>
+            </th>
+        </tr>
+        @endforeach
+    </table>
+</div>
 @endsection
