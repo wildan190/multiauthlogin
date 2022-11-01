496344907979ad0fb5bda28a1f5f7b5ed54266b0
wildanbelfiore <mikewildan@outlook.com>
2022-11-01 12:12:56 +0700

    new file:   resources/views/inven/create.blade.php 	modified:   resources/views/inven/index.blade.php

---
+++ resources/views/inven/create.blade.php
@@ -0,0 +1,5 @@
+@extends('dashboards.admins.layouts.admin-dash-layout')
+@section('title','Inventory')
+@section('content')
+Test Test Test
+@endsection
--- resources/views/inven/index.blade.php
+++ resources/views/inven/index.blade.php
@@ -1,5 +1,88 @@
 @extends('dashboards.admins.layouts.admin-dash-layout')
-@section('title','leavemanagement')
+@section('title','Inventory')
 @section('content')
-Test Test Test
+<style>
+.headline{
+    margin-left: 10px;
+    margin-top: 10px;
+    font-family: Arial, Helvetica, sans-serif;
+}
+.table1{
+    display: inline-block;
+    margin-left: 20px;
+    margin-top: 5px;
+}
+.table2{
+display: inline-block;
+margin-left: 10px;
+margin-top: 5px;
+}
+.table3{
+    display: inline-block;
+    margin-left: 10px;
+    margin-top: 5px;
+}
+.table4{
+    margin-left: auto;
+    margin-right: auto;
+    background-color: #C6EBC5;;
+    padding-bottom: -20px;
+    padding-top: -30px;
+    text-align: center;
+}
+.link1{
+    display: block;
+    margin-left: 20px;
+    margin-top: 10px;
+    margin-bottom: 5px;
+    color: black;
+}
+.tr1{
+    background-color: #367E18;
+    color: white;
+}
+
+</style>
+
+<h4 class="headline">Inventory Barang</h4>
+<!--button start -->
+<table class="table1">
+    <tr>
+        <th><a class="btn btn-success btn-sm" href="#">+ Add</a></th>
+    </tr>
+</table>
+<table class="table2">
+    <Tr>
+        <th><a class="btn btn-info btn-sm" href="">Update</a></th>
+    </Tr>
+</table>
+<table class="table3">
+    <tr>
+        <th><a class="btn btn-danger btn-sm" href="">Delete</a></th>
+    </tr>
+</table>
+<!--button end -->
+
+<a href="" class="link1">Export Report</a>
+
+<table class="table4" cellpadding="30" >
+    <tr class="tr1">
+        <th>Id</th>
+        <th>Kategori</th>
+        <th>Nama Barang</th>
+        <th>Jumlah Barang</th>
+        <th>Tanggal Input</th>
+        <th>Note</th>
+    </tr>
+    <tr>
+    <th>1</th>
+    <th>2</th>
+    <th>3</th>
+    <th>4</th>
+    <th>5</th>
+    <th>6</th>
+ </tr>
+</table>
+
+
 @endsection
