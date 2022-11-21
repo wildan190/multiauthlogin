c55558dfc2e8a14f8b215e4814bb2738a96924e3
wildanbelfiore <mikewildan@outlook.com>
2022-11-21 15:21:40 +0700

    modified:   resources/views/dashboards/admins/index.blade.php 	modified:   resources/views/dashboards/users/index.blade.php

--- resources/views/dashboards/users/index.blade.php
+++ resources/views/dashboards/users/index.blade.php
@@ -3,6 +3,29 @@
 
 @section('content')
 
-User Page
+<!--
+<style>
+    .table1 {
+        margin-top: 5px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    #submit1 {
+        margin-left: 10px;
+        margin-top: 10px;
+    }
+</style>
+@section('content')
+<button type="submit" class="btn btn-warning" id="submit1">Export </button>
+<table class="table1">
+    <tr>
+        <td><img src="https://i.ibb.co/WHpGYsc/965ea843-afd6-428d-bcce-a0aaa290cd90.jpg" alt=""></td>
+    </tr>
+</table>
+<script>
+    alert("data masih berupa sample")
+</script>
+-->
 
 @endsection
